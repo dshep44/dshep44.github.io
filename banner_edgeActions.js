9ddf8a556229f01a7423e5e3b8e72ff2252a503b
dshep44 <dshep44@live.com.au>
2017-05-31 22:52:30 +1000

    Version 10
    Overhaul

---
+++ banner_edgeActions.js
@@ -0,0 +1,14 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",14250,function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})(jQuery,AdobeEdge,"EDGE-104625233");
